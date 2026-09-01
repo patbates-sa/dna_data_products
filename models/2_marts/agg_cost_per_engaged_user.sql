d34a37f9831c705a393b4db47f67af096f045b98
Pat Bates <pat.bates@dbtlabs.com>
2026-09-01 03:44:56 +0000

    merged the marketing project into dna_data_products

---
+++ models/2_marts/agg_cost_per_engaged_user.sql
@@ -0,0 +1,24 @@
+with engaged_users as (
+    select * from {{ ref('int_engaged_users') }}
+),
+
+ad_spend_per_adv as (
+    select * from {{ ref('dim_ad_spend_per_adv') }}
+),
+
+cost_per_engaged_user as (
+    select
+        e.user_id,
+        e.total_engagement,
+        a.total_ad_spend
+    from engaged_users as e
+    inner join ad_spend_per_adv as a
+        on e.user_id = a.advertiser_id
+)
+
+select
+    user_id,
+    total_engagement,
+    total_ad_spend,
+    total_ad_spend / nullif(total_engagement, 0) as cost_per_engaged_user
+from cost_per_engaged_user
