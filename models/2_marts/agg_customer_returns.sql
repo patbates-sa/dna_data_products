1d3890ba584dffcb40cf0acb6414500a797f7379
Pat Bates <pat.bates@dbtlabs.com>
2026-09-02 15:44:57 +0000

    cleaned up demo artifacts for next demo

--- models/2_marts/agg_customer_returns.sql
+++
@@ -1,8 +0,0 @@
-select 
-    customer_key
-    , count(distinct order_key) as total_orders
-    , sum(gross_item_sales_amount) as total_sales_amount
-from {{ ref('stg_fct_order_items') }}
-where is_return = true
-group by 1
-order by 1 desc
